3f03c395df52caa5edfe574546c89fe36cf2b7fb
mehmet Dağ <you@example.com>
2023-01-11 16:45:03 +0300

    first commit

---
+++ src/paket1/C8.java
@@ -0,0 +1,8 @@
+package paket1;
+
+public class C8 {
+    public static void main(String[] args) {
+        C7 obje=new C7();
+        obje.işlemler();
+    }
+}
---
+++ src/paket2/C8.java
@@ -0,0 +1,17 @@
+package paket2;
+
+public class C8 {
+    private int sayı=3;
+
+    public int getSayı() {
+        return sayı;
+    }
+
+    public void setSayı(int sayı) {
+        this.sayı = sayı;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
